ae9251800cb38b1ba0d70d3889768fb75ede0dfe
jjesic <jjesic6680@gmail.com>
2021-07-22 14:24:02 +0900

    QR코드 합본

---
+++ app/src/main/java/com/example/test_db/qrcode/MainActivityQR.java
@@ -0,0 +1,39 @@
+package com.example.test_db.qrcode;
+
+import android.content.Intent;
+//import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.test_db.R;
+
+
+public class MainActivityQR extends AppCompatActivity {
+    private Button createQRBtn;
+    private Button scanQRBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_qr);
+
+        createQRBtn = (Button) findViewById(R.id.createQR);
+        scanQRBtn = (Button) findViewById(R.id.scanQR);
+
+        createQRBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivityQR.this, CreateQR.class);
+                startActivity(intent);
+            }
+        });
+
+        scanQRBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivityQR.this, ScanQR.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
